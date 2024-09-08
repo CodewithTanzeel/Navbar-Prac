42996aac6ee79e98e86fe27cf8586b3a7186eed5
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-08 12:03:08 +0500

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { WavyBackground } from "@/components/ui/wavy-background";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,9 +26,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased` }
       >
+        
         {children}
       </body>
     </html>
